5810eb309e5ef05a15025533e963a9dd7f69fe3c
simmiagrawal24 <s_agrawal@bt.iitr.ac.in>
2023-12-20 21:39:50 +0530

    initial

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose  = require('mongoose')
+
+
+const mongoURI = "mongodb://localhost:27017/?directConnection=true"
+
+const connectToMongo = async () => {
+    mongoose.connect(mongoURI, await console.log("Connected to mongo Successful")
+       );
+   }
+  
+
+module.exports = connectToMongo;
